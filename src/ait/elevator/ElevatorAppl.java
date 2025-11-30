aac09800225c9ae1338216204cd5b3b613ea3c26
Alexey Moor <asmodeus.1819@gmail.com>
2025-11-30 08:45:23 +0100

    Add support for multiple elevators in Truck and ElevatorAppl classes

--- src/ait/elevator/ElevatorAppl.java
+++ src/ait/elevator/ElevatorAppl.java
@@ -9,10 +9,12 @@ public class ElevatorAppl {
     private static final int CAPACITY = 20; // in kg
 
     static void main(String[] args) throws InterruptedException {
-        Elevator elevator = new Elevator("V.I.Lenin");
+        Elevator elevator1 = new Elevator("'V.I.Lenin'");
+        Elevator elevator2 = new Elevator("'I.V.Stalin'");
+
         Truck[] trucks = new Truck[N_TRUCKS];
         for (int i = 0; i < trucks.length; i++) {
-            trucks[i] = new Truck(N_RACES, CAPACITY, elevator);
+            trucks[i] = new Truck(N_RACES, CAPACITY, elevator1, elevator2);
         }
         Thread[] threads = new Thread[trucks.length];
         for (int i = 0; i < threads.length; i++) {
@@ -22,6 +24,8 @@ public class ElevatorAppl {
         for (Thread thread : threads) {
             thread.join();
         }
-        System.out.println("Elevator " + elevator.getName() + " has " + elevator.getCurrentVolume() + " kg");
+        System.out.println("Elevator 1: " + elevator1.getName() + " has " + elevator1.getCurrentVolume() + " kg");
+        System.out.println("Elevator 2: " + elevator2.getName() + " has " + elevator2.getCurrentVolume() + " kg");
+        System.out.println("Total: " + (elevator1.getCurrentVolume() + elevator2.getCurrentVolume()) + " kg");
     }
 }
--- src/ait/elevator/task/Truck.java
+++ src/ait/elevator/task/Truck.java
@@ -3,23 +3,31 @@ package ait.elevator.task;
 import ait.elevator.model.Elevator;
 
 public class Truck implements Runnable {
-    private static final Object monitor = new Object();
+    private static final Object monitor1 = new Object();
+    private static final Object monitor2 = new Object();
+
     private final int nRaces;
     private final int capacity; // in kg
-    private final Elevator elevator;
+    private final Elevator elevator1;
+    private final Elevator elevator2;
 
-    public Truck(int nRaces, int capacity, Elevator elevator) {
+    public Truck(int nRaces, int capacity, Elevator elevator1, Elevator elevator2) {
         this.nRaces = nRaces;
         this.capacity = capacity;
-        this.elevator = elevator;
+        this.elevator1 = elevator1;
+        this.elevator2 = elevator2;
     }
-
 
     @Override
     public void run() {
+        int halfCapacity = capacity / 2;
+
         for (int i = 0; i < nRaces; i++) {
-            synchronized (monitor) {
-                elevator.add(capacity);
+            synchronized (monitor1) {
+                elevator1.add(halfCapacity);
+            }
+            synchronized (monitor2) {
+                elevator2.add(halfCapacity);
             }
         }
     }
