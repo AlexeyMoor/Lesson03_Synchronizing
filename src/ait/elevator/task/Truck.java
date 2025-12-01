9a4634ab9a3255e65e7e49c847616de8dac10d93
Alexey Moor <asmodeus.1819@gmail.com>
2025-12-01 14:24:15 +0100

    Add reentrant lock to synchronize access to elevator in Truck class

--- src/ait/elevator/task/Truck.java
+++ src/ait/elevator/task/Truck.java
@@ -2,7 +2,11 @@ package ait.elevator.task;
 
 import ait.elevator.model.Elevator;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Truck implements Runnable {
+    private static final Lock mutex = new ReentrantLock();
     private final int nRaces;
     private final int capacity; // in kg
     private final Elevator elevator;
@@ -17,7 +21,12 @@ public class Truck implements Runnable {
     @Override
     public void run() {
         for (int i = 0; i < nRaces; i++) {
-            elevator.add(capacity);
+            mutex.lock();
+            try {
+                elevator.add(capacity);
+            } finally {
+                mutex.unlock();
+            }
         }
     }
 }
